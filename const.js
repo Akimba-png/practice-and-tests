3bb3bdf01fe4ce1e83217b444cbbaf47c2e2c88c
Oleg <akimba@gmail.com>
2022-12-12 21:22:43 +0300

    feat: Добавляет файл исходных констант

---
+++ const.js
@@ -0,0 +1,13 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const unsortedArr = [4, 1, 3, 7, 5, 2, 8, 0, 10, 9, 8, 11, 6];
+const nestedArray = [1, [2, 3], 4, 5, [6, [7, [8, [9, 10], 11], 12], 13], 14];
+const floatArr = [1.22, 2.44, 2.57, 4.48, 7.99, 5.42, 4.34, 7.85, 4.23, 7.09, 8.56];
+const repeatedArr = ['abc', 'abc', 'def', 'def', 'def', 'ghi', 'ghi', 'ghi', 'ghi'];
+const obj = {
+  type: 'dog',
+  stat: {
+    name: 'pluto',
+    age: 5,
+  },
+  values: [1, 2, [3, 4], 5],
+};
