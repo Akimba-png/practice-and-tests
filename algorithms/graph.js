2d6974492be9468a9223010b58b3419dfe966c96
Oleg <akimba@gmail.com>
2022-12-23 13:44:46 +0300

    alg: Добавялет алгоритм dijkstra search

--- algorithms/graph.js
+++ algorithms/graph.js
@@ -24,3 +24,55 @@ function breadthSearch(graph, start, end) {
 const result = breadthSearch(graph, 'a', 'g');
 console.log(result);
 }
+
+
+// dijkstra Search для поиска минимального пути в графе
+const graph = {
+  a: {b: 7, c: 2},
+  b: {f: 5},
+  c: {d: 4, e: 1},
+  d: {f: 3},
+  e: {f: 2},
+  f: {g: 1},
+  g: {},
+};
+
+function dijkstraSearch(graph, start) {
+  const costTable = {};
+  const processed = [];
+  let neighbour = {};
+
+  Object.keys(graph).forEach((node) => {
+    if (node !== start) {
+      costTable[node] = graph[start][node] ?? Infinity;
+    }
+  });
+
+  let cheapestNode = getCheapestNode(costTable, processed);
+
+  while (cheapestNode) {
+    neighbour = graph[cheapestNode];
+    Object.keys(neighbour).forEach((node) => {
+      costTable[node] = costTable[cheapestNode] + neighbour[node];
+    });
+    processed.push(cheapestNode);
+    cheapestNode = getCheapestNode(costTable, processed);
+  }
+
+  return costTable;
+}
+
+function getCheapestNode(costTable, processed) {
+  let cheapestNode;
+  let cheapestCost = Infinity;
+  Object.keys(costTable).forEach((node) => {
+    if (costTable[node] < cheapestCost && !processed.includes(node)) {
+      cheapestNode = node;
+      cheapestCost = costTable[node];
+    }
+  });
+  return cheapestNode;
+}
+
+const result = dijkstraSearch(graph, 'a');
+console.log(result);
